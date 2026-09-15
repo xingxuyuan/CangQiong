4285d1477d40e3d66e9dfa790057558332144feb
xingxuyuan <3523053932@qq.com>
2026-09-16 00:56:04 +0800

    苍穹
    苍穹

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -26,11 +26,8 @@ public interface DishMapper {
 
     void deleteById(Long id);
 
-    /**
-     * 根据分类id查询菜品数量
-     * @param categoryId
-     * @return
-     */
     @Select("select count(*) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
+
+    List<Dish> list(Dish dish);
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -17,6 +17,7 @@ import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -37,6 +38,7 @@ public class SetmealServiceImpl implements SetmealService {
     @Autowired
     private DishMapper dishMapper;
 
+    
     @Override
     @Transactional
     public void saveWithDish(SetmealDTO setmealDTO) {
@@ -115,5 +117,16 @@ public class SetmealServiceImpl implements SetmealService {
                 .updateUser(BaseContext.getCurrentId())
                 .build();
         setmealMapper.update(setmeal);
+    }
+
+
+    @Override
+    public List<Setmeal> list(Setmeal setmeal) {
+        return setmealMapper.list(setmeal);
+    }
+
+    @Override
+    public List<DishItemVO> getDishItemById(Long id) {
+        return setmealMapper.getDishItemBySetmealId(id);
     }
 }
